d87bc910cd1148fa1317990cf30e7907dacc422c
rtzgod <azimaidynuly@gmail.com>
2024-08-03 17:44:41 +0500

    bst

---
+++ binaryTrees/BST/main.go
@@ -0,0 +1,25 @@
+package main
+
+func main() {
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	} else if root.Val < val {
+		return searchBST(root.Right, val)
+	} else {
+		return root
+	}
+
+}
